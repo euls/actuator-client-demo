060ed5f66d298d86d4c917d8eae224e2d2544fc0
elus <1315065950@qq.com>
2020-06-05 09:49:18 +0800

    first commit

---
+++ src/test/java/com/example/actuatorclientdemo/ActuatorClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuatorclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
